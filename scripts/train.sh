6b2f59b2ab616ff29417afbe1a2b899463d4aa1a
shota takashiro <takashiro@weblab.t.u-tokyo.ac.jp>
2023-02-06 23:38:49 +0900

    コマンドライン引数で指定できるように修正

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,24 @@
-dataset_dir=./dataset/packaged
-task=put_rubbish_in_bin
-seed=0
+CMDNAME=`basename $0`
+if [ $# -ne 5 ]; then
+  echo "Usage: $CMDNAME dataset_dir task seed variations device" 1>&2
+  exit 1
+fi
+# sh scripts/train.sh ../shota.takashiro/dataset2 put_rubbish_in_bin 0 0 cuda:0
+dataset_dir=$1
+task=$2
+seed=$3
+variations=$4
+device=$5
+
+CMDNAME=`basename $0`
+
+echo $dataset_dir, $task, $seed, $variations, $device
 
 python train.py \
 	--tasks $task \
-	--dataset $dataset_dir/$seed \
-	--num_workers 2  \
- 	--instructions ./dataset/instructions/$task/instructions.pkl \
-	--variations 0 \
-    --device "cuda:0" \
-	--train_iters 10000
+	--dataset $dataset_dir/packaged/$seed \
+	--num_workers 8  \
+ 	--instructions $dataset_dir/instructions/$task/instructions.pkl \
+	--variations $variations \
+  	--device $device \
+	--train_iters 100000
