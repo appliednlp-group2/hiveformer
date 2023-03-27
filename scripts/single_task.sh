0abb6aab3aa5b754c1b070e046557a7ba51abc09
Shota Takashiro <takashiro@weblab.t.u-tokyo.ac.jp>
2023-03-28 00:15:50 +0900

    baselineとfixedを切り替えられるように修正

--- scripts/single_task.sh
+++ scripts/single_task.sh
@@ -4,7 +4,7 @@ task=slide_block_to_target
 cuda=(cuda:0 cuda:0 cuda:1)
 for seed in 0 1 2
 do 
-    mkdir xp/hiveformer/$task/$seed
-    nohup sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset $task $seed 0 ${cuda[$seed]} > xp/hiveformer/$task/$seed/log.txt &
+    mkdir -p xp/hiveformer/$task/$seed
+    nohup sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset $task $seed 0 instructions_fixed ${cuda[$seed]} > xp/hiveformer/$task/$seed/log.txt &
 done
 # sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset put_rubbish_in_bin 0 0 cuda:0 
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 CMDNAME=`basename $0`
-if [ $# -ne 5 ]; then
-  echo "Usage: $CMDNAME dataset_dir task seed variations device" 1>&2
+if [ $# -ne 6 ]; then
+  echo "Usage: $CMDNAME dataset_dir task seed variations instruction device" 1>&2
   exit 1
 fi
 # sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset put_rubbish_in_bin 0 0 cuda:0 
@@ -8,17 +8,18 @@ dataset_dir=$1
 task=$2
 seed=$3
 variations=$4
-device=$5
+instruction=$5
+device=$6
 
 CMDNAME=`basename $0`
 
-echo $dataset_dir, $task, $seed, $variations, $device
+echo $dataset_dir, $task, $seed, $variations, $instruction, $device
 
 CUDA_LAUNCH_BLOCKING=1 python train.py \
 	--tasks $task \
 	--dataset $dataset_dir/packaged/0 \
 	--num_workers 2  \
- 	--instructions $dataset_dir/instructions/$task/instructions.pkl \
+ 	--instructions $dataset_dir/instructions/$task/$instruction.pkl \
 	--variations $variations \
   	--device $device \
 	--train_iters 100000 \
