2f8c0fd3146a8e544a7ba9a2c0584111a96e10fb
takashiro <takashiro@weblab.t.u-tokyo.ac.jp>
2023-03-28 15:51:46 +0900

    single_task.shを修正

--- scripts/single_task.sh
+++ scripts/single_task.sh
@@ -1,11 +1,11 @@
 # tasks=(basketball_in_hoop slide_block_to_target wipe_desk lamp_off close_drawer turn_tap take_usb_out_of_computer turn_oven_on close_microwave)
 
-task=wipe_desk
-cuda=(cuda:0 cuda:1 cuda:2)
+task=close_drawer
+cuda=(cuda:0 cuda:0 cuda:1)
 instruction=instructions_fixed
 for seed in 0 1 2
 do 
-    mkdir -p xp/hiveformer/$task/$seed
+    mkdir -p xp/hiveformer/$instruction/$task/$seed
     nohup sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset $task $seed 0 $instruction ${cuda[$seed]} > xp/hiveformer/$instruction/$task/$seed/log.txt &
 done
 # sh scripts/train.sh /root/hiveformer_dataset/multi_task_dataset put_rubbish_in_bin 0 0 cuda:0 
